a7e97ed946bdfe415aba2c4451b42f92a48a0c08
Death Gun24 <miguel_guzman6@usmp.pe>
2024-05-25 20:03:34 -0500

    primer commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Practicatresss.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
