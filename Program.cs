a1351bce77f7e56f0465bd40bee8ae312faf0114
Death Gun24 <miguel_guzman6@usmp.pe>
2024-05-25 20:43:10 -0500

    segundo commit

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Practicatresss.Data;
+using PC3API.Integration.API;
+using Microsoft.Extensions.Logging;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,6 +15,8 @@ builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddScoped<APIIntegration, APIIntegration>();
 
 var app = builder.Build();
 
